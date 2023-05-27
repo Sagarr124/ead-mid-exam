8167e7f56241a410c4f8ada938b1843139c478b9
Sagar <sagarkph@gmail.com>
2023-05-27 17:45:08 +0500

    Add AnotherUserParticipation Component

---
+++ src/TASK-A/components/AnotherUserParticipation.jsx
@@ -0,0 +1,7 @@
+import { Button } from "antd";
+
+function AnotherUserParticipation(props) {
+  return <Button onClick={props.clickMethod}>New User</Button>;
+}
+
+export default AnotherUserParticipation;
